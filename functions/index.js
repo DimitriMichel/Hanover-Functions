37b06cfe0201fa83a6ca8764223671df902f84db
Dimitri PL <dimitrimachines@gmail.com>
2020-05-01 20:47:15 -0400

    Validation Errors

--- functions/index.js
+++ functions/index.js
@@ -42,6 +42,7 @@ firebase.initializeApp(firebaseConfig);
 
 // DB Document References
 const stocksRef = db.collection("stocks");
+const usersRef = db.collection("users");
 
 // Routes
 // Get all user stocks
@@ -82,6 +83,18 @@ app.post("/stock", (request, response) => {
     });
 });
 
+//Registration Helpers
+const isEmpty = (string) => {
+  if (string.trim() === "") return true;
+  else return false;
+};
+
+const isEmail = (email) => {
+  const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (email.match(regEx)) return true;
+  else return false;
+};
+
 //User Registration
 app.post("/register", (request, response) => {
   const newUser = {
@@ -91,9 +104,32 @@ app.post("/register", (request, response) => {
     userName: request.body.userName,
   };
 
-  //TODO Validate Data
+  //Validators & Errors Object
+  const errors = {};
+  if (isEmpty(newUser.userName)) {
+    errors.userName = "Cannot be empty.";
+  }
+  if (isEmpty(newUser.email)) {
+    errors.email = "Cannot be empty.";
+  } else if (!isEmail(newUser.email)) {
+    errors.email = "Must be a valid email address";
+  }
+  if (isEmpty(newUser.password)) {
+    errors.password = "Cannot be empty.";
+  }
+  if (newUser.password !== newUser.confirmPassword) {
+    errors.confirmPassword = "Passwords do not match";
+  }
+  console.log(errors);
+
+  //Check for any errors in Errors object
+  if (Object.keys(errors).length > 0) {
+    return response.status(400).json(errors);
+  }
+
   let token, userID;
-  db.doc(`/users/${newUser.userName}`)
+  usersRef
+    .doc(`users/${newUser.userName}`)
     .get()
     .then((doc) => {
       if (doc.exists) {
